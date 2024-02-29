2fbdd5850245b661ba606ecbd24f63139104cbf2
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-03-01 01:11:47 +0700

    Update page.php

--- routes/admin/page.php
+++ routes/admin/page.php
@@ -3,12 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Sokeio\Page\Livewire\PageForm;
 use Sokeio\Page\Livewire\PageTable;
-use Sokeio\Page\PageBuilder;
 
 Route::group(['as' => 'admin.'], function () {
     route_crud('page', PageTable::class, PageForm::class);
     if (page_with_builder()) {
-        Route::get('page/create-builder', PageBuilder::class)->name('page.create-builder');
-        Route::get('page/{dataId}/edit-builder', PageBuilder::class)->name('page.edit-builder');
+        Route::get('page/create-builder', route_theme(Sokeio\Page\PageBuilder::class))->name('page.create-builder');
+        Route::get('page/{dataId}/edit-builder', route_theme(Sokeio\Page\PageBuilder::class))->name('page.edit-builder');
     }
 });
