335c1ac351914877eb8a960dbe9b1db8e2334a9f
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-03-01 01:15:36 +0700

    Update function.php

--- helpers/function.php
+++ helpers/function.php
@@ -4,6 +4,6 @@
 if (!function_exists('page_with_builder')) {
     function page_with_builder()
     {
-        return function_exists('module_active') && !!!module_active('builder');
+        return function_exists('module_active') && !!module_active('builder');
     }
 }
