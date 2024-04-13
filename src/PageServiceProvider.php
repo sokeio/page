3da4e14a6eae090494424d846ac1d0158526127a
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-04-13 21:55:41 +0700

    Update PageServiceProvider.php

--- src/PageServiceProvider.php
+++ src/PageServiceProvider.php
@@ -111,14 +111,7 @@ class PageServiceProvider extends ServiceProvider
                         ->route(
                             ['name' => 'admin.page', 'params' => []],
                             'Pages',
-                            '<svg xmlns="http://www.w3.org/2000/svg"
-                        class="icon icon-tabler icon-tabler-brand-pagekit"
-                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
-                        stroke="currentColor" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <path d="M12.077 20h-5.077v-16h11v14h-5.077"></path>
-         </svg>',
+                            '<i class="ti ti-wallpaper fs-2"></i>',
                             [],
                             'admin.page'
                         );
