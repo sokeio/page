9f7a3506d18bd05ad218c27f4f1f85978f39501a
nguyen van hau <nguyenvanhau.soft@gmail.com>
2025-02-12 21:55:50 +0700

    Update PageView.php

--- src/PageView.php
+++ src/PageView.php
@@ -16,27 +16,28 @@ class PageView extends \Sokeio\Page
 
     public function render()
     {
-        if (!$this->slug && setting('SOKEIO_SITE_HOMEPAGE') === null) {
+        $homepageId = setting('SOKEIO_SITE_HOMEPAGE');
+        if (!$this->slug && !$homepageId) {
             return  Theme::view('sokeio-page::pages.homepage');
         }
         $page = null;
         if ($this->slug) {
             $page =  Slug::findSluggableBySlug($this->slug, Page::class);
-            Theme::title($page->title);
-            Theme::description($page->description);
-            if ($page->id == setting('SOKEIO_SITE_HOMEPAGE')) {
+
+            if ($page?->id == $homepageId) {
                 $this->redirectRoute('site.page');
             }
+            Theme::title($page?->title);
+            Theme::description($page?->description);
         } else {
-            $page = Page::find(setting('SOKEIO_SITE_HOMEPAGE'));
-            Theme::title(setting('SOKEIO_SITE_TITLE', $page->title));
-            Theme::description(setting('SOKEIO_SITE_DESCRIPTION', $page->description));
+            $page = Page::find($homepageId);
+            Theme::title(setting('SOKEIO_SITE_TITLE', $page?->title));
+            Theme::description(setting('SOKEIO_SITE_DESCRIPTION', $page?->description));
         }
         if (!$page) {
             return abort(404);
         }
-        $viewPage = 'sokeio-page::pages.page.view';
-        return Theme::view($viewPage, [
+        return Theme::view('sokeio-page::pages.page.view', [
             'page' => $page
         ], [], false, $page->template);
     }
